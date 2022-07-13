6b7a78089884990244f6b761a45ebda6a7746c4f
Usman Gavandi <74993022+usman5445@users.noreply.github.com>
2022-07-13 23:57:11 +0530

    Update orders.js

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default function orderRequest() {
     axios
-      .get("/orders")
+      .get(`${process.env.BACKEND_URL}/orders`)
       .then((resp) => console.log(resp))
       .catch((err) => console.log(err));
   }
